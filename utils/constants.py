3b12f56a4dec6f115075ffc69096938f4246e7c8
Raphael Junior <raphnano@gmail.com>
2018-12-17 10:55:02 -0500

    Code quality and additional testing.

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,8 @@ GITHUB_USER_URL = 'https://api.github.com/user'
 GITHUB_AUTH = 'https://api.github.com/authorizations/clients/:4f435d9f7beb896578b9'
 REPOS = GITHUB_USER_URL + '/repos'
 GITHUB_REPO = "https://api.github.com/repos/"
+environment = {
+    "owner": "ADI-Labs",
+    "repo": "culpa2",
+    "pulls": 2
+}
